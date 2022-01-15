28a92716171747a62b92bf728023d834db442e73
Jennifer161500 <getwild31@gmail.com>
2022-01-16 00:33:28 +0900

    First commit

---
+++ first.ts
@@ -0,0 +1,12 @@
+let myname:string | number = 'kim';
+myname = "lee";
+myname = 123;
+
+let mylist:string[] = ['apple','banana'];
+
+type myvar = string|number;
+
+function myfunc(x:number): number{
+    return x*2
+}
+myfunc(123)
